8de1f710bae2abed017e1e9b9ed51affb4c27587
Florian Pinault <floriankrb@users.noreply.github.com>
2025-05-26 15:51:56 +0200

    feat: implement worker to delete datasets on s3 (#85)

--- src/anemoi/registry/workers/delete_dataset.py
+++ src/anemoi/registry/workers/delete_dataset.py
@@ -9,7 +9,6 @@
 
 
 import logging
-import os
 
 from anemoi.registry.entry.dataset import DatasetCatalogueEntry
 
@@ -36,47 +35,34 @@ class DeleteDatasetWorker(Worker):
 
         self.platform = platform
         self.filter_tasks.update(filter_tasks)
-        self.filter_tasks["platform"] = self.platform
+
+        # TODO: location and platform should be made consistent in the catalogue
+        self.filter_tasks["location"] = self.platform
 
     def worker_process_task(self, task):
         platform, dataset = self.parse_task(task)
         entry = DatasetCatalogueEntry(key=dataset)
         assert platform == self.platform, (platform, self.platform)
 
-        locations = entry.record.get("locations", {})
-        if platform not in locations:
-            LOG.warning(f"Dataset {dataset} has no locations on '{platform}'. Ignoring delete request.")
-            return
         if self.dry_run:
-            LOG.warning(f"Would delete {locations[platform]['path']} from '{platform}' but this is only a dry run.")
+            LOG.warning(
+                f"Would delete {entry.record['locations'][platform]['path']} from '{platform}' but this is only a dry run."
+            )
             return
 
-        path = locations[platform]["path"]
-        LOG.warning(f"Deleting {path} from '{platform}'")
-
-        tmp_path = path + ".deleting"
-        i = 0
-        while os.path.exists(tmp_path):
-            i += 1
-            tmp_path = path + ".deleting." + str(i)
-        os.rename(path, tmp_path)
-
-        # shutil.rmtree(tmp_path)
-        LOG.warning(f"Deleted {path} from '{platform}'")
-
-        entry.remove_location(platform)
-        LOG.warning(f"Removed location from catalogue {path} from '{platform}'")
+        entry.delete_location(platform)
 
     @classmethod
     def parse_task(cls, task):
         assert task.record["action"] == "delete-dataset", task.record["action"]
 
-        platform, dataset = super().parse_task(task, "platform", "dataset")
+        # TODO: location and platform should be made consistent in the catalogue
+        platform, dataset = super().parse_task(task, "location", "dataset")
 
         if "/" in platform:
-            raise ValueError(f"Platform {platform} must not contain '/', this is a platform name")
+            raise ValueError(f"platform {platform} must not contain '/', this is a platform name")
         if "." in platform:
-            raise ValueError(f"Platform {platform} must not contain '.', this is a platform name")
+            raise ValueError(f"platform {platform} must not contain '.', this is a platform name")
 
         if "." in dataset:
             raise ValueError(f"The dataset {dataset} must not contain a '.', this is the name of the dataset.")
